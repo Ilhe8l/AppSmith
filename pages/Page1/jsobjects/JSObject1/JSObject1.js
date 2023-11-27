8dd68f10e522846f2036e45834582bccb8816f50
Guilherme Souza <guilhermesouza3278@gmail.com>
2023-11-27 20:59:56 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,15 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	myFun2 () {
+		
+		
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+	
+}
